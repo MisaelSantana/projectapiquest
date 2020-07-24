a6cefc78831d68c8be1703fc0b922494f2bc6f8e
MisaelSantana <misaelsantana2011@hotmail.com>
2020-07-23 21:06:50 -0300

    modificação do projeto, e deleta do spectre.css

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'spectre.css/dist/spectre.min.css'
-import 'spectre.css/dist/spectre-icons.min.css'
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
